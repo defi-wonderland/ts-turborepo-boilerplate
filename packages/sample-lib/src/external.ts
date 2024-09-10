c793f655590df39ba86450f3e0e97d6dca8e741f
nigiri <168690269+0xnigir1@users.noreply.github.com>
2024-09-10 09:27:36 -0300

    feat: sample blockchain lib

---
+++ packages/sample-lib/src/external.ts
@@ -0,0 +1,6 @@
+export type { IBlockchainProvider } from "./internal.js";
+export type { Address } from "viem";
+
+export { InvalidRpcUrl } from "./internal.js";
+
+export { BlockchainProvider } from "./internal.js";
---
+++ packages/sample-lib/src/internal.ts
@@ -0,0 +1,3 @@
+export * from "./exceptions/index.js";
+export * from "./interfaces/index.js";
+export * from "./providers/index.js";
