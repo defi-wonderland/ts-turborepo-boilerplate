4a8d4da957e2cb23d966bb7d0cf423082ad8f458
nigiri <168690269+0xnigir1@users.noreply.github.com>
2024-09-10 10:40:32 -0300

    feat: sample app

---
+++ apps/sample-app/src/config/env.ts
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import { z } from "zod";
+
+dotenv.config();
+
+const validationSchema = z.object({
+    RPC_URL: z.string().url(),
+});
+
+const env = validationSchema.safeParse(process.env);
+
+if (!env.success) {
+    console.error(env.error.issues.map((issue) => JSON.stringify(issue)).join("\n"));
+    process.exit(1);
+}
+
+export const environment = env.data;
+export type Environment = z.infer<typeof validationSchema>;
